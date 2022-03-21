93493725227182f12d01274bb1448f3866ed5911
Marco Saccarola <marco.sacca.ads@gmail.com>
2022-03-21 13:20:05 +0100

    Server connection to Mongo

---
+++ src/server.js
@@ -0,0 +1,29 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import cors from 'cors'
+import listEndpoints from 'express-list-endpoints'
+
+const server=express()
+const port=process.env.PORT||3001
+
+// ****************************** MIDDLEWARES
+server.use(cors())
+server.use(express.json())
+
+// ****************************** ROUTES
+
+
+// ****************************** CONNECTION
+mongoose.connect(process.env.MONGO_CONNECTION)
+mongoose.connection.on('connected',()=>{
+    console.log('CONNECTED TO MONGO DB')
+    server.listen(port,()=>{
+        console.table(listEndpoints(server))
+        console.log(`SERVER RUNNING ON PORT ${port}`)
+    })
+})
+mongoose.connection.on('error',(err)=>{
+    console.log(err)
+})
+
+
