40c510d4600ebc19224948c70f839f97d83bc941
Marco Saccarola <marco.sacca.ads@gmail.com>
2022-03-21 15:30:58 +0100

    User schema: pw hash, credentials check, hidding object values

--- src/services/users/schema.js
+++ src/services/users/schema.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose'
+import bcrypt from 'bcrypt'
 
 const{Schema,model}=mongoose
 
@@ -12,5 +13,36 @@ const userSchema=new Schema(
     },
     {timestamps:true}
 )
+
+//* HASHING PW, BEFORE SAVING IN DB
+userSchema.pre('save',async function(next){
+    const newUser=this // THIS refers to the current user
+    const plainPW=newUser.pw
+    //* IT HASH ONLY IF USER IS MODIFYING PW VALUE
+    if(newUser.isModified('pw')){
+        newUser.pw=await bcrypt.hash(plainPW,10)
+    }
+    next()
+})
+
+//* CHECKING CREDENTIALS
+userSchema.statics.checkCredentials=async function(email,plainPW){
+    const user=await this.findOne({email}) // THIS refers to the userModel
+    if(user){
+        const isMatch=await bcrypt.compare(plainPW,user.pw)
+        if(isMatch) return user
+        else return null // if pw not matched
+    } else return null // if user not found
+}
+
+//* HIDDING SOME OBJECT VALUES
+userSchema.methods.toJSON=function(){
+    const userDocument=this
+    const userObject=userDocument.toObject()
+    delete userObject.pw
+    delete userObject.__v
+    delete userObject.createdAt
+    delete userObject.updatedAt
+}
 
 export default model('user',userSchema)
